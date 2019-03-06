d59de7f281f163e29fc0c7b669bbc2a314384441
Dmitry Olshansky <olshanskydr@gmail.com>
2019-03-06 23:36:50 +0300

    Add: insert_

--- pg-schema/src/Database/PostgreSQL/DML/Insert.hs
+++ pg-schema/src/Database/PostgreSQL/DML/Insert.hs
@@ -7,6 +7,7 @@ import Database.PostgreSQL.DB
 import Database.PostgreSQL.Simple
 import Database.Schema.Def
 import Database.Schema.Rec
+import GHC.Int
 
 
 -- TODO
@@ -18,18 +19,30 @@ insertSch
   => Connection -> [r] -> IO [r']
 insertSch conn = returning conn (insertText @sch @t @r @r')
 
+insertSch_
+  :: forall sch t r
+  . (CQueryRecord PG sch t r, ToRow r)
+  => Connection -> [r] -> IO Int64
+insertSch_ conn = executeMany conn (insertText_ @sch @t @r)
+
 insertText
   :: forall sch t r r'. (CQueryRecord PG sch t r, CQueryRecord PG sch t r')
   => Query
-insertText = "insert into " `mappend` sn `mappend` "." `mappend` tn
+insertText = insertText_ @sch @t @r `mappend` " returning " `mappend` fs'
+  where
+    qr' = getQueryRecord @PG @sch @t @r'
+    fs' = fromString $ T.unpack
+      $ T.intercalate "," [ dbn | (FieldPlain _ dbn _) <- queryFields qr']
+
+insertText_
+  :: forall sch t r. CQueryRecord PG sch t r => Query
+insertText_ = "insert into " `mappend` sn `mappend` "." `mappend` tn
   `mappend` "(" `mappend` fs `mappend` ") values (" `mappend` qs
-  `mappend` ") returning " `mappend` fs'
+  `mappend` ")"
   where
     qr = getQueryRecord @PG @sch @t @r
-    qr' = getQueryRecord @PG @sch @t @r'
     (fs,qs) = bimap inter inter
       $ unzip [ (dbn,"?") | (FieldPlain _ dbn _) <- queryFields qr]
-    fs' = inter [ dbn | (FieldPlain _ dbn _) <- queryFields qr']
     toQ = fromString . T.unpack
     tn = toQ $ tableName qr
     sn = toQ $ schemaName @sch
