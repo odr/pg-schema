23b8394a91819f243afa9cd9d466b4dc3055159f
odr <dima@typeable.io>
2025-05-13 22:49:12 +0300

    add imports

--- pg-schema/src/PgSchema.hs
+++ pg-schema/src/PgSchema.hs
@@ -1,19 +1,18 @@
 module PgSchema (module PGS) where
 
 import Database.PostgreSQL.Convert as PGS
-import Database.PostgreSQL.DML.Insert as PGS
-import Database.PostgreSQL.DML.InsertJSON as PGS
-import Database.PostgreSQL.DML.Update as PGS
-import Database.PostgreSQL.DML.Select as PGS
-import Database.PostgreSQL.DML.Select.Types as PGS
 import Database.PostgreSQL.Enum as PGS
 import Database.PostgreSQL.PgTagged as PGS
-import Database.PostgreSQL.Schema.Catalog as PGS
+import Database.PostgreSQL.DML.Delete as PGS
+import Database.PostgreSQL.DML.Insert as PGS
+import Database.PostgreSQL.DML.Insert.Types as PGS
+import Database.PostgreSQL.DML.InsertJSON as PGS
+import Database.PostgreSQL.DML.Select as PGS
+import Database.PostgreSQL.DML.Select.Types as PGS
+import Database.PostgreSQL.DML.Update as PGS
 import Database.PostgreSQL.Schema.Schema as PGS
-import Database.PostgreSQL.Schema.TH as PGS (mkSchema)
 import Database.Schema.Def as PGS
 import Database.Schema.Rec as PGS
 import Database.Schema.TH as PGS
 import Database.Types.SchList as PGS
 import PgSchema.Util as PGS (ToStar)
-import Util.TH.LiftType as PGS
