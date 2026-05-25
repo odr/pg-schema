0e5579cbef2ae9f08fc6443ef71d320da5fca002
odr <dima@typeable.io>
2026-05-25 11:37:55 +0300

    more Generic in tests (Conditions)

--- pg-schema/test-pgs/Tests/Conditions.hs
+++ pg-schema/test-pgs/Tests/Conditions.hs
@@ -15,7 +15,7 @@ import Data.Ord
 import Data.Pool as Pool
 import Data.Text as T
 import Database.PostgreSQL.Simple
-import GHC.Generics
+import GHC.Generics (Generic)
 import GHC.Int
 import GHC.Stack (HasCallStack)
 import Hedgehog
@@ -23,20 +23,10 @@ import PgSchema.DML
 import Utils
 
 
-data RootI = MkRootI
-  { code :: Text
-  , grp :: Int32
-  , someEmpty :: ()
-  , name :: Text }
-  deriving (Show, Generic)
-  deriving anyclass GenDefault
-
-data Mid1I = MkMid1I
-  { pos :: Int32
-  , flag :: Bool
-  , sort_key :: Int32
-  , payload :: Maybe Text }
-  deriving (Show, Generic, Eq, Ord)
+data LeafI = MkLeafI
+  { leafNo :: Int32
+  , value :: Double }
+  deriving stock (Eq, Ord, Show, Generic)
   deriving anyclass GenDefault
 
 data Mid2I = MkMid2I
@@ -44,28 +34,51 @@ data Mid2I = MkMid2I
   , kind :: Text
   , flag :: Bool
   , priority :: Int32 }
-  deriving (Show, Generic, Eq, Ord)
+  deriving stock (Eq, Ord, Show, Generic)
   deriving anyclass GenDefault
 
-data LeafI = MkLeafI
-  { leaf_no :: Int32
-  , value :: Double }
-  deriving (Generic, Eq, Ord, Show)
+data Mid1I = MkMid1I
+  { pos :: Int32
+  , flag :: Bool
+  , sortKey :: Int32
+  , payload :: Maybe Text }
+  deriving stock (Eq, Ord, Show, Generic)
   deriving anyclass GenDefault
 
-eqRoot :: RootI -> RootI -> Bool
-eqRoot r1 r2 = (r1.code, r1.grp) == (r2.code, r2.grp)
+data RootI = MkRootI
+  { code :: Text
+  , grp :: Int32
+  , someEmpty :: ()
+  , name :: Text }
+  deriving stock (Show, Generic)
+  deriving anyclass GenDefault
 
 type InsData = "mid1_root_fk" := [Mid1I]
   :. "mid2_root_fk" := ["leaf_mid2_fk" := [LeafI] :. Mid2I]
   :. RootI
+
+-- | Two edges in the result type that renamer maps to the same DB fk
+-- ('mid1_root_fk2' -> 'mid1_root_fk'); each branch gets its own 'qPath' filter.
+data RootDualMid1 = MkRootDualMid1
+  { code :: Text
+  , grp :: Int32
+  , someEmpty :: ()
+  , name :: Text
+  , mid1RootFk :: [Mid1I] -- Renamer renames it to "mid1_root_fk"
+  , mid1_root_fk2 :: [Mid1I] -- Renamer renames it to "mid1_root_fk"
+  }
+  deriving stock (Show, Generic)
+  deriving anyclass GenDefault
+
+eqRoot :: RootI -> RootI -> Bool
+eqRoot r1 r2 = (r1.code, r1.grp) == (r2.code, r2.grp)
 
 insData :: MonadIO m => Pool Connection -> PropertyT m [InsData]
 insData pool = do
   rootsIn <- forAll (L.nubBy eqRoot <$> genData' RootI 1 200)
   mid1In <- forAll (genData' Mid1I 0 10)
   mid2In <- forAll (L.nubBy ((==) `on` (.seq)) <$> genData' Mid2I 0 10)
-  leafIn <- forAll (L.nubBy ((==) `on` (.leaf_no)) <$> genData' LeafI 0 10)
+  leafIn <- forAll (L.nubBy ((==) `on` (.leafNo)) <$> genData' LeafI 0 10)
   let
     inIns = rootsIn <&> \r -> "mid1_root_fk" =: mid1In
       :. "mid2_root_fk" =: (mid2In <&> \m2 -> "leaf_mid2_fk" =: leafIn :. m2)
@@ -95,20 +108,12 @@ prop_cond_query pool = withTests 30 $ property do
           qWhere $ pparent (TS "leaf_mid2_fk")
             $ pparent (TS "mid2_root_fk") $ "grp" >? (0::Int32)
         qOrderBy [descf "kind"]
-  L.length (L.filter (\(PgTag m1s :. _ :. r) ->
-    (r.grp > 100 || r.grp `L.elem` [0..70])
-    && L.any ((<100) . (.sort_key)) (L.take 2 $ L.sortBy (comparing (.pos)) m1s)
-    )
-    inIns) === L.length res
-
-type OutData = "mid2_root_fk" := [Mid2I] :. InsData
-
--- | Two edges in the result type that renamer maps to the same DB fk
--- ('mid1_root_fk2' -> 'mid1_root_fk'); each branch gets its own 'qPath' filter.
-type RootDualMid1 =
-  "mid1_root_fk" := [Mid1I]
-  :. "mid1_root_fk2" := [Mid1I]
-  :. RootI
+  L.length (L.filter rootMatches inIns) === L.length res
+  where
+    rootMatches (PgTag m1s :. _ :. r) =
+      (r.grp > 100 || r.grp `L.elem` [0..70])
+      && L.any ((<100) . (.sortKey))
+        (L.take 2 $ L.sortBy (comparing (.pos)) m1s)
 
 -- | 'qPath' / 'qWhere' on 'mid1_root_fk' vs 'mid1_root_fk2' must not be merged
 -- just because both edges share one fk in the database.
@@ -121,38 +126,36 @@ prop_renamer_alias_dual_fields pool = withTests 30 $ property do
         qWhere $ "flag" =? True
       qPath "mid1_root_fk2" do
         qWhere $ "pos" >? (5 :: Int32)
-  forM_ sel \case
-    PgTag viaFk :. PgTag viaFk2 :. root ->
-      let
-        expectFk = [ m1
-          | PgTag m1s :. _ :. r <- inIns
-          , eqRoot r root
-          , m1 <- m1s
-          , m1.flag
-          ]
-        expectFk2 = [ m1
-          | PgTag m1s :. _ :. r <- inIns
-          , eqRoot r root
-          , m1 <- m1s
-          , m1.pos > 5
-          ]
-      in do
-        L.sort viaFk === L.sort expectFk
-        L.sort viaFk2 === L.sort expectFk2
+  forM_ sel \root -> do
+    let
+      expectFk = [ m1
+        | PgTag m1s :. _ :. r <- inIns
+        , (r.code, r.grp) == (root.code, root.grp)
+        , m1 <- m1s
+        , m1.flag
+        ]
+      expectFk2 = [ m1
+        | PgTag m1s :. _ :. r <- inIns
+        , (r.code, r.grp) == (root.code, root.grp)
+        , m1 <- m1s
+        , m1.pos > 5
+        ]
+    L.sort root.mid1RootFk === L.sort expectFk
+    L.sort root.mid1_root_fk2 === L.sort expectFk2
 
--- We can have similar pathes in select.
--- Params are applied for all branches with this path
+-- | Duplicate 'mid2_root_fk' in the result type: filters on the path
+-- and on nested 'leaf_mid2_fk' apply per branch and may diverge.
 prop_cond_by_dup_path :: Pool Connection -> Property
 prop_cond_by_dup_path pool = withTests 30 $ property do
   inIns <- insData pool
-  (sel :: [OutData], _) <- evalIO $ withResource pool \conn ->
+  (sel :: ["mid2_root_fk" := [Mid2I] :. InsData], _) <- evalIO $ withResource pool \conn ->
     selSch "root" conn $ qRoot
-      $ qPath "mid2_root_fk" do
+      $ qPath "mid2_root_fk" do -- there are two "mid2_root_fk" branches
         qWhere $ "flag" =? True
-        qPath "leaf_mid2_fk" do
-          qWhere $ "leaf_no" >? (100::Int32)
+        qPath "leaf_mid2_fk" do -- only one "leaf_mid2_fk" branch
+          qWhere $ "leaf_no" >? (97::Int32)
   L.sort [m2 | (PgTag m2s :. _) <- sel, m2 <- m2s]
     === L.sort [m2 | (_ :. PgTag m2s :. _) <- inIns, (_ :. m2) <- m2s, m2.flag]
   L.sort [(m2, L.length ls) | (_ :. _ :. PgTag m2s :. _) <- sel, (PgTag ls :. m2) <- m2s]
-    === L.sort [(m2, L.length $ L.filter ((>100) . (.leaf_no)) ls)
+    === L.sort [(m2, L.length $ L.filter ((>97) . (.leafNo)) ls)
       | (_ :. PgTag m2s :. _) <- inIns, (PgTag ls :. m2) <- m2s, m2.flag]
